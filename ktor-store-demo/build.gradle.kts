8e0460208b5e6cb479e1e37f9440e771fbb02889
Nate Ebel <n8ebel@goobar.dev>
2022-11-02 21:58:45 -0600

    Starter code for 05_05

--- ktor-store-demo/build.gradle.kts
+++ ktor-store-demo/build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-call-logging:$ktor_version")
     
-    
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
---
+++ ktor-store-demo/src/main/kotlin/com/linkedinlearning/model/Product.kt
@@ -0,0 +1,13 @@
+package com.linkedinlearning.model
+
+data class Product(
+    val upc: String,
+    val name: String,
+    val description: String
+)
+
+val IN_MEMORY_PRODUCTS = listOf(
+    Product("abc123", "Travel Backpack 20L", "A mis-sized backpack for your travel needs"),
+    Product("aabbcc", "Passport Protector", "Keep your passport safe"),
+    Product("123456", "Fancy Carry On Bag", "A stylish carry on back for all your travels")
+)
