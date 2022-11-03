727057066f2f813f264588a4e1bade8ba50344c6
Nate Ebel <n8ebel@goobar.dev>
2022-11-02 22:09:33 -0600

    Sample code for 05_07

--- ktor-store-demo/src/main/kotlin/com/linkedinlearning/plugins/Routing.kt
+++ ktor-store-demo/src/main/kotlin/com/linkedinlearning/plugins/Routing.kt
@@ -1,8 +1,11 @@
 package com.linkedinlearning.plugins
 
+import com.linkedinlearning.model.Product
 import com.linkedinlearning.persistence.ProductDatabase
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 
 fun Application.configureRouting() {
@@ -14,6 +17,16 @@ fun Application.configureRouting() {
 
         get("/products") {
             call.respond(ProductDatabase.dao.products())
+        }
+
+        post("/add-product") {
+            val body = call.receive<Product>()
+
+            when(val insertedProduct = ProductDatabase.dao.addProduct(body)) {
+                null -> call.respondText("Failed to add product: $body", status = HttpStatusCode.InternalServerError)
+                else -> call.respondText("Added product: $body", status = HttpStatusCode.Created)
+            }
+
         }
     }
 }
